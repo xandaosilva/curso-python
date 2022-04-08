ea96dc73b15d191c08827be18ac2505473b8f0fd
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-04-08 07:37:50 -0300

    Varriáveis

---
+++ aula06/aula06.py
@@ -0,0 +1,8 @@
+nome = "Alexandre"
+idade = 29
+altura = 1.70
+peso = 110
+maior_de_idade = idade > 18
+imc = peso / (altura**2)
+
+print(nome, "tem" ,idade , "anos de idade e sua altura é" , altura , "m e seu imc é", imc, sep=" ")
