1d8f1ba790058a741711f1b1e046fbef066e7c44
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-06-20 16:22:31 -0300

    While - estrutura de repetição em Python

---
+++ capitulo01/aula17/aula17.py
@@ -0,0 +1,19 @@
+x = 0
+y = 0
+
+while x <= 100:
+    if x % 10 == 0:
+        print(x)
+
+    x+=2
+
+while y <= 5:
+    if y == 3:
+        print(y)
+        break
+
+    y += 1
+
+
+while True:
+    print("Estou com fome !!!")
