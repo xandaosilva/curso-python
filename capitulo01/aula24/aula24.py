a528bc8b37cbe2bbfcde215a2f32b339859c35d1
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-06-22 11:12:17 -0300

    Expressão condicional com operador OR

--- capitulo01/aula24/aula24.py
+++ capitulo01/aula24/aula24.py
@@ -1,8 +1,10 @@
 logged_user = True
 age = 18
+user_name = input("Digite o nome de usuário: ")
 
 message = "Usuário logado." if logged_user else "Usuário não logado"
 access_message = "Acesso permitido." if age >= 18 else "Acesso negado"
 
 print(message)
 print(access_message)
+print(user_name or "Você não digitou o nome de usuário")
