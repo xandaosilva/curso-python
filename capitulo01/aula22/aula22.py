4f23e2c450e476bb7f454e6ee103d325a419499b
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-06-22 10:16:28 -0300

    Desempacotamento de listas

--- capitulo01/aula22/aula22.py
+++ capitulo01/aula22/aula22.py
@@ -4,9 +4,13 @@ listA = text.split(" ")
 listB = text.split(",")
 listC = "#".join(text)
 
+a, b, c, *listD = text;
+
 print(listA)
 print(listB)
 print(listC)
 
 for indice, valor in enumerate(listA):
     print(indice, valor)
+
+print(a, b, c, listD)
