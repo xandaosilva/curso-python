0676d6b20f631000e4230a20de0dcc73a9d802b1
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-11-13 20:20:23 -0300

    LogFileMixin

--- capitulo05/aula08/app.py
+++ capitulo05/aula08/app.py
@@ -1,1 +1,14 @@
-from log import log
+from log import Log, LogFileMixin, LogPrintMixin
+
+log = Log()
+logFileMixin = LogFileMixin()
+logPrintMixin = LogPrintMixin()
+
+# log.log("teste log")
+logFileMixin.log("teste file mixin")
+logFileMixin.error_message("teste file mixin erro")
+logFileMixin.success_message("teste file mixin sucesso")
+
+logPrintMixin.log("teste print mixin")
+logPrintMixin.error_message("teste print mixin erro")
+logPrintMixin.success_message("teste print mixin sucesso")
--- capitulo05/aula08/log.py
+++ capitulo05/aula08/log.py
@@ -1,19 +1,29 @@
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / "log.txt"
+
 class Log:
     def log(self, message):
         raise NotImplementedError("Implemente o método log")
 
     def error_message(self, message):
         return self.log(f"Erro: {message}")
+    
+    def success_message(self, message):
+        return self.log(f"Sucesso: {message}")
 
 
 class LogFileMixin(Log):
     def log(self, message):
-        print(message)
+        msg = f"{message} ({self.__class__.__name__})"
+        with open(LOG_FILE, "a") as file:
+            file.write(msg)
+            file.write("\n")
 
 
 class LogPrintMixin(Log):
     def log(self, message):
-        print(f"{message} {self.__class__.__name__}")
+        print(f"{message} ({self.__class__.__name__})")
 
 
 if __name__ == "__main__":
@@ -21,7 +31,13 @@ if __name__ == "__main__":
     logFileMixin = LogFileMixin()
     logPrintMixin = LogPrintMixin()
     
-    log.log("teste logo")
+    # log.log("teste log")
     logFileMixin.log("teste file mixin")
+    logFileMixin.error_message("teste file mixin erro")
+    logFileMixin.success_message("teste file mixin sucesso")
+
     logPrintMixin.log("teste print mixin")
+    logPrintMixin.error_message("teste print mixin erro")
+    logPrintMixin.success_message("teste print mixin sucesso")
+    print(LOG_FILE)
 
