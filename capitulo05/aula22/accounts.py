17556f385332f0b87255e0ec1b975e6a3f2da2c2
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-11-20 18:00:08 -0300

    Exercício Contas - Classe Current Account

--- capitulo05/aula22/accounts.py
+++ capitulo05/aula22/accounts.py
@@ -1,7 +1,7 @@
 import abc
 
 class Account(abc.ABC):
-    def __init__(self, agency, account_number, balance):
+    def __init__(self, agency, account_number, balance=0):
         self.agency = agency
         self.account_number = account_number
         self.balance = balance
@@ -11,21 +11,38 @@ class Account(abc.ABC):
 
     def deposit(self, value):
         self.balance += value
-        self.details(f"Depósito no valor de {value} foi realizado com sucesso.")
+        self.details(f"Depósito no valor de R${value:.2f} foi realizado com sucesso.")
 
     def details(self, message=""):
         print(f"{message} O seu saldo atual é R${self.balance:.2f}")
 
     def __str__(self) -> str:
-        return f"Informações da conta\nAgência: {self.agency}\nConta: {self.account_number}\nSaldo: R${self.balance}"
+        return f"Informações da conta\nAgência: {self.agency}\nConta: {self.account_number}\nSaldo: R${self.balance:.2f}"
 
 
 class SavingAccount(Account):
     def withdraw(self, value):
         if self.balance - value >= 0:
             self.balance -= value
-            self.details(f"Saque no valor de {value} foi realizado com sucesso.")
+            self.details(f"Saque no valor de R${value:.2f} foi realizado com sucesso.")
             return self.balance
         
-        self.details(f"Saque no valor de {value} não realizado.")
+        self.details(f"Saque no valor de R${value:.2f} não realizado.")
+
+
+class CurrentAccount(Account):
+    def __init__(self, agency, account_number, balance=0, limit=0):
+        super().__init__(agency, account_number, balance)
+        self.limit = limit
+
+    def withdraw(self, value):
+        if self.balance - value >= -self.limit:
+            self.balance -= value
+            self.details(f"Saque no valor de R${value:.2f} foi realizado com sucesso.")
+            return self.balance
+        
+        self.details(f"Saque no valor de R${value:.2f} não realizado pois seu limite é R${-self.limit:.2f}.")
+
+    def __str__(self) -> str:
+        return f"{super().__str__()}\nLimite: R${-self.limit:.2f}"
 
--- capitulo05/aula22/app.py
+++ capitulo05/aula22/app.py
@@ -1,8 +1,6 @@
-from accounts import SavingAccount
+from accounts import SavingAccount, CurrentAccount
 
 sa1 = SavingAccount(123, 1, 300)
-
-print(sa1)
 
 sa1.deposit(700)
 sa1.deposit(1000)
@@ -10,3 +8,10 @@ sa1.withdraw(1700)
 sa1.withdraw(5000)
 
 print(sa1)
+
+ca1 = CurrentAccount(456, 100, 1000, 100)
+
+ca1.deposit(2000)
+ca1.withdraw(3200)
+
+print(ca1)
