c0addfd905b4b0a97d2188dc49741579949f9011
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-11-20 18:15:16 -0300

    Exercício Contas - Classe Person

---
+++ capitulo05/aula22/persons.py
@@ -0,0 +1,22 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
+    
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+
