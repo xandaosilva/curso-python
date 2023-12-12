76557178846e35abb0496e79361cd693c958bebe
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-12-12 15:33:22 -0300

    Projeto Calculadora: ajustes no projeto

--- capitulo07/aula03/app.py
+++ capitulo07/aula03/app.py
@@ -1,19 +1,16 @@
 import sys
 from main_window import MainWindow
-from PySide6.QtWidgets import QApplication, QLabel
-
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication
+from variables import WINDOW_ICON_PATH
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+    icon = QIcon(str(WINDOW_ICON_PATH))
 
-    label_a = QLabel("Label")
-    label_a.setStyleSheet("font-size: 150px")
-    
-    window.v_layout.addWidget(label_a)
-    window.setWindowTitle("Calculadora")
+    app.setWindowIcon(icon)
     window.adjustFixedSize()
-
     window.show()
-    app.exec()
 
+    app.exec()
